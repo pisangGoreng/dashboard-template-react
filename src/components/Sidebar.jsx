c4d58b28554ee6537935d4dc01d0241008bedbf4
endy <7ofpentacles@gmail.com>
2023-03-10 13:55:10 +0700

    add sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -94,8 +94,9 @@ function SideBar({
             <Box m="1.5rem 2rem 2rem 3rem">
               <FlexBetween color={theme.palette.secondary.main}>
                 <Box display="flex" alignItems="center" gap="0.5rem">
-                  <Typography variant="h4" fontWeight="bold">
-                    Ecomvision
+                  <Typography variant="h5" fontWeight="bold">
+                    {process.env.REACT_APP_SIDEBAR_NAME} -{" "}
+                    {process.env.REACT_APP_STAGE}
                   </Typography>
                 </Box>
 
